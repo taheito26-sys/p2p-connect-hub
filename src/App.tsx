3b11c32024f20e8f3a62e8bad10f05f2e531f449
taheito26-sys <taheito26@gmail.com>
2026-03-21 02:18:54 +0300

    Configure Clerk JWT template for API auth

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <AuthProvider>
             <Routes>
               <Route path="/auth/login" element={<LoginPage />} />
